10820c7325b5dc214e917f5b142ad7fa4e4879a1
Code Wrecker <janbrylle.estomo-21@cpu.edu.ph>
2025-02-18 08:55:33 +0800

    first commit

---
+++ simple_script.js
@@ -0,0 +1,9 @@
+// A simple JavaScript file
+
+// Function to display a greeting message
+function greet(name) {
+    return `Hello, ${name}! Welcome to JavaScript.`;
+}
+
+// Example usage
+console.log(greet("Janbrylle"));
